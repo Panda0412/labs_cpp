304863862cb173d4b057d9838a2a39bd69c2feba
Panda0412 <nasb_99@mail.ru>
2020-07-04 00:03:56 +0300

    implemented class safe_c_array_t: - constructors and destructor - assignment operator - operator[]

---
+++ lab2/lab2_b.cpp
@@ -0,0 +1,42 @@
+#include "lab2_b.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    // tests
+    int size = 5;
+
+    safe_c_array_t<int> arr1 = safe_c_array_t<int>(size);
+
+    cout << "size = " << arr1.get_size() << endl;
+
+    cout << endl << "arr1 (without definition)" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << "i = " << i << ": " << arr1[i] << endl;
+    }
+
+    safe_c_array_t<int> arr2(size);
+
+    for (int i = 0; i < size; ++i) {
+        arr2[i] = i + 1;
+    }
+
+    cout << endl << "arr2" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << "i = " << i << ": " << arr2[i] << endl;
+    }
+
+    safe_c_array_t<int> arr3(arr2);
+
+    cout << endl << "arr3 (equal arr2)" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << "i = " << i << ": " << arr3[i] << endl;
+    }
+
+    arr1 = arr2;
+
+    cout << endl << "arr1 after assignment (arr1 = arr2)" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << "i = " << i << ": " << arr1[i] << endl;
+    }
+}
---
+++ lab2/lab2_b.h
@@ -0,0 +1,43 @@
+#ifndef LABS_CPP_LAB2_B_H
+#define LABS_CPP_LAB2_B_H
+
+
+template <class T>
+class safe_c_array_t {
+private:
+    size_t size;
+    T *arr;
+public:
+    //constructor
+    explicit safe_c_array_t(size_t new_size): size(new_size), arr(new T[size]) { }
+    //copy constructor
+    safe_c_array_t(const safe_c_array_t &s_c_a_t) {
+        size = s_c_a_t.size;
+        arr = new T[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = s_c_a_t[i];
+        }
+    }
+
+    //size getter
+    size_t get_size() { return size; }
+
+    //assignment operator
+    safe_c_array_t& operator=(const safe_c_array_t<T> &r) {
+        size = r.size;
+        for (int i = 0; i < size; ++i) {
+            arr[i] = r[i];
+        }
+        return *this;
+    }
+
+    //operator[]
+    T& operator[] (const size_t index) { return arr[index]; }
+    const T& operator[] (const size_t index) const { return arr[index]; }
+
+    //destructor
+    ~safe_c_array_t() { delete[] arr; }
+};
+
+
+#endif //LABS_CPP_LAB2_B_H
